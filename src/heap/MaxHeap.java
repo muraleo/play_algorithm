1dd06d209cc4ce87162c5af06e62352eaef708a3
muraleo <muraleo80923@gmail.com>
2017-08-03 17:53:59 -0400

    Maxheap:three constructor

--- src/heap/MaxHeap.java
+++ src/heap/MaxHeap.java
@@ -3,13 +3,24 @@ import java.lang.Math.*;
 
 public class MaxHeap<E extends Comparable>{
 	private static final int CAPACITY = 1000; //default capacity
-	public E[] data; //inner array
+	private E[] data; //inner array
 	private int sz = 0; //indicate the amount of element in heap
 
 	//Constructor
 	MaxHeap(){this(CAPACITY+1);} //default constructor, have 1000 capacity, invoke next constructor
 	MaxHeap(int n){		 //parameter constructor, have custom capacity
 		data = (E[]) new Comparable[n+1]; //remember + 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	}
+	MaxHeap(E[] arr){
+		int n = arr.length;
+		data = (E[]) new Comparable[n+1];
+		for(int i = 1; i <= arr.length; i++){
+			data[i] = arr[i-1];
+		}
+		sz = n;
+		for(int i = n/2; i>0;i--){
+			shiftDown(i);
+		}
 	}
 
 	//access method
@@ -45,7 +56,8 @@ public class MaxHeap<E extends Comparable>{
 	public E poll() throws IndexOutOfBoundsException{
 		if(sz<1) throw new IndexOutOfBoundsException("queue is empty");
 		E result = data[1];
-		data[1] = data[--sz];
+		data[1] = data[sz];
+		sz--;
 		shiftDown(1);	
 		return result;	
 	}
@@ -58,19 +70,30 @@ public class MaxHeap<E extends Comparable>{
 		}
 	}
 
-	private void shiftDown(int i){
-		while(sz>1&&2*i<=sz){ //hava to make sure i, 2i and 2i+1 are all exist at the same time!
-			if(data[2*i].compareTo(data[2*i+1])>0){
-				if(data[i].compareTo(data[2*i])<0){
-					Helper.swap(data, i, 2*i);
-					i+=i;
-				}
-			}else{
-				if(data[i].compareTo(data[2*i+1])<0){
-					Helper.swap(data, i, 2*i+1);
-					i=2*i+1;
-				}
-			}
+	private void shiftDown(int k){
+		// while(2*i<=sz){ //hava to make sure i, 2i and 2i+1 are all exist at the same time!
+		// 	System.out.println("1");
+		// 	if(2*i+1<=sz&&(data[2*i].compareTo(data[2*i+1])>0)){
+		// 		if(data[i].compareTo(data[2*i])<0){
+		// 			Helper.swap(data, i, 2*i);
+		// 			i=2*i;
+		// 		}
+		// 		System.out.println("2");
+		// 	}else{
+		// 		if(2*i+1<=sz&&(data[i].compareTo(data[2*i+1])<0)){
+		// 			Helper.swap(data, i, 2*i+1);
+		// 			i=2*i+1;
+		// 		}
+		// 		System.out.println("3");
+		// 	}
+		// }
+		while(2*k<=sz){
+			int j = 2*k;
+			if(j+1<=sz && data[j+1].compareTo(data[j])>0)
+				j++;
+			if(data[k].compareTo(data[j])>0) break;
+			Helper.swap(data,k,j);
+			k=j;
 		}
 	}
 
