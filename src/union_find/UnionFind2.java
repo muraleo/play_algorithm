8efe4ea48e32264cf13e4fba2121eee43203a6ba
muraleo <muraleo80923@gmail.com>
2017-08-04 21:43:13 -0400

    use parent array to optimize union operation

---
+++ src/union_find/UnionFind2.java
@@ -0,0 +1,37 @@
+public class UnionFind2{
+	private int[] parent; //parent int array to store parent of each element
+	private int size;
+
+	//constructor
+	//create a new unionfind with size n
+	public UnionFind2(int n){
+		this.parent = new int[n];
+		this.size = n;
+		for(int i = 0; i<n; i++){
+			parent[i] = i;
+		}
+	}
+
+	//return p's root
+	public int find(int p) throws IndexOutOfBoundsException{
+		if(p<0 || p>=size) throw new IndexOutOfBoundsException("p is out of bound");
+		while(p!=parent[p]){
+			p = parent[p]; //search p's parent
+		}
+		return p; //return p's parent
+	}
+
+	//check if p and q are connected
+	public boolean isconnected(){
+		return find[p] == find[q];
+	}
+
+	//union p and q
+	public void union(){
+		int pRoot = find(p); //remember to use variable to save operation
+		int qRoot = find(q);
+		if(pRoot == qRoot)
+			return;
+		parent[p]=qRoot; //now the union operation is O(1)
+	}
+}
