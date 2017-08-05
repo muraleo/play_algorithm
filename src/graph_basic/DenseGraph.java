e5a31d6cc8970edb077599493ff7e9df03860793
muraleo <muraleo80923@gmail.com>
2017-08-05 01:36:38 -0400

    dense graph adjacency matrix

---
+++ src/graph_basic/DenseGraph.java
@@ -0,0 +1,37 @@
+public class DenseGraph{
+	private int n; // number of nodes
+	private int m; // number of edges
+	private boolean directed; // mark whether a directed graph
+	private boolean[][] g; //graph
+
+	//constructor
+	public DenseGraph(int n, boolean direct){
+		assert n>0;
+		this.n = n;
+		this.m = 0;
+		this.directed = direct;
+		g = new boolean[n][n];
+	}
+
+	//access method
+	public int V(){return n;} //get number of node
+	public int E(){return m;} //get number of edge
+
+	//update method
+	public void addEdge(int v, int w){
+		assert v>=0||v<n;
+		assert w>=0||w<m;
+		if(hasEdge(v, w))
+			return;
+		g[v][w] = true;
+		if(directed==false)
+			g[w][v] = true;
+		m++;
+	}
+
+	public boolean hasEdge(int v, int w){
+		assert v>=0||v<n;
+		assert w>=0||w<m;
+		return g[v][w];
+	}
+}
